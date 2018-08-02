a7071f3618b5a66f5e5a948e0fd998521ca3c1ed
Fayoke Adeyina <fayokeadeyina@Fayokes-MacBook-Pro.local>
2018-08-02 14:23:58 +0100

    return token with authenticated user response

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,10 @@
-
-import { Router } from 'express';
+import {
+  Router
+} from 'express';
 import userValidator from '../middlewares/validations/UserValidation';
 import AuthController from '../controllers/AuthController';
 import passport from '../auth/authStrategies';
+import JwtHelper from '../helpers/JwtHelper';
 
 const authRouter = Router();
 
@@ -19,35 +21,75 @@ authRouter.get('/google', passport.authenticate('google', {
 authRouter.get('/google/callback', (req, res, next) => {
   passport.authenticate('google', (err, user, info) => {
     if (err) {
-      return res.redirect('/');
+      return res.send(err);
     }
     if (!user) {
-      return res.status(401).send({ status: 401, success: false, message: 'Google Authentication Failed' });
+      return res.status(401).send({
+        status: 401,
+        success: false,
+        message: 'Google Authentication Failed'
+      });
     }
     req.login(user, (loginErr) => {
       if (loginErr) {
         return next(loginErr);
       }
-      return res.status(200).send({ status: 200, success: true, message: 'Google Authentication Succeeded' });
+      const token = JwtHelper.createToken({
+        id: user.id,
+        email: user.email
+      }, '24h');
+      return res.status(200).send({
+        status: 200,
+        success: true,
+        message: 'Google Authentication Succeeded',
+        user: {
+          username: user.username,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          token,
+        }
+      });
     });
   })(req, res, next);
 });
 
-authRouter.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+authRouter.get('/facebook', passport.authenticate('facebook', {
+  scope: ['email']
+}));
 
-authRouter.get('/facebook/callback',(req, res, next) => {
+authRouter.get('/facebook/callback', (req, res, next) => {
   passport.authenticate('facebook', (err, user, info) => {
     if (err) {
-      return res.redirect('/');
+      return res.send('err');
     }
     if (!user) {
-      return res.status(401).send({ status: 401, success: false, message: 'Facebook Authentication Failed' });
+      return res.status(401).send({
+        status: 401,
+        success: false,
+        message: 'Facebook Authentication Failed'
+      });
     }
     req.login(user, (loginErr) => {
       if (loginErr) {
         return next(loginErr);
       }
-      return res.send({ success: true, message: 'Facebook Authentication Succeeded' });
+      const token = JwtHelper.createToken({
+        id: user.id,
+        email: user.email
+      }, '24h');
+      return res.status(200).send({
+        status: 200,
+        success: true,
+        message: 'Facebook Authentication Succeeded',
+        user: {
+          username: user.username,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          token,
+        },
+      });
     });
   })(req, res, next);
 });
