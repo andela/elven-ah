d6761c40115ab6d5e764bf67d59aee51a2ede7b8
Fayoke Adeyina <fayokeadeyina@Fayokes-MacBook-Pro.local>
2018-08-06 23:33:24 +0100

    create models and association for article, tag, comment, reply and rating

---
+++ server/migrations/20180806205733-create-tag.js
@@ -0,0 +1,34 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Tags', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    articleId: {
+      type: Sequelize.INTEGER,
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+      references: {
+        model: 'Articles',
+        key: 'id',
+        as: 'articleId'
+      },
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Tags')
+};
---
+++ server/migrations/20180806205756-create-reply.js
@@ -0,0 +1,39 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Replies', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    articleId: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    commentId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+      references: {
+        model: 'Comments',
+        key: 'id',
+        as: 'commentId'
+      },
+    },
+    body: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Replies')
+};
