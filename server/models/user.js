70f2c637be9dee39a9bcd3467175c1ce12fc2e19
Fayoke Adeyina <fayokeadeyina@Fayokes-MacBook-Pro.local>
2018-08-06 23:33:24 +0100

    create models and association for article, tag, comment, reply and rating

--- server/models/user.js
+++ server/models/user.js
@@ -14,19 +14,42 @@ export default (sequelize, DataTypes) => {
           msg: 'Must be a valid email address',
         },
       },
-      unique: true
-    },
-    googleId: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    facebookId: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    twitterId: {
-      type: DataTypes.STRING,
-      allowNull: true,
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      googleId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      facebookId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      twitterId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      verified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      bio: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
   });
 
