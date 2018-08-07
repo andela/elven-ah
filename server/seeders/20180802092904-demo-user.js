8e5caa04311691ad829adfd28645e1a68616eede
Oyomi <seayomi@gmail.com>
2018-08-07 15:59:55 +0100

    feat(passwordReset): rebase changes into password reset branch
    - rebase and refactor the codebase for travis to build successfully
    
    [Delivers #159204718]

--- server/seeders/20180802092904-demo-user.js
+++ server/seeders/20180802092904-demo-user.js
@@ -6,6 +6,8 @@ module.exports = {
       username: 'oyomi',
       email: 'seayomi@gmail.com',
       verified: true,
+      createdAt: '2018-08-01 21:54:49',
+      updatedAt: '2018-08-04 21:54:49',
     }
   ], {}),
 
