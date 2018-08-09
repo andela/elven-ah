9979c939b66b4f8145642ab4d2f2a76dda2656f8
Fayoke Adeyina <fayokeadeyina@Fayokes-MacBook-Pro.local>
2018-08-09 14:14:43 +0100

    modify comment and reply models and migrateion

---
+++ server/seeders/20180806182424-demo-user.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  up: queryInterface => queryInterface.bulkInsert('Users', [
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      username: 'unique',
+      email: 'testseeder@test.com',
+      bio: `John Doe was born in 1977 when he arrived in Los Angeles. 
+          His previous life in Tennessee, 
+          Wisconsin & Baltimore was a great & fertile time but 
+          new music and social changes led him to events that created a life in art.`,
+      image: 'https://www.image.com/example/image/john',
+      createdAt: '2018-08-08 18:31:22.324',
+      updatedAt: '2018-08-08 18:31:22.324'
+    }
+  ]),
+  down: () => {
+  }
+};
