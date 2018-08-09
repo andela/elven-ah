4d803cd4a10a2984acc8e7f6a9d41c00a1af81b8
Fayoke Adeyina <fayokeadeyina@Fayokes-MacBook-Pro.local>
2018-08-09 14:14:43 +0100

    modify comment and reply models and migrateion

--- server/seeders/20180806182424-demo-user.js
+++ server/seeders/20180806182424-demo-user.js
@@ -2,6 +2,15 @@
 module.exports = {
   up: queryInterface => queryInterface.bulkInsert('Users', [
     {
+      firstName: 'Yomi',
+      lastName: 'Doe',
+      username: 'oyomi',
+      email: 'seayomi@gmail.com',
+      verified: true,
+      createdAt: '2018-08-01 21:54:49',
+      updatedAt: '2018-08-04 21:54:49',
+    },
+    {
       firstName: 'John',
       lastName: 'Doe',
       username: 'unique',
