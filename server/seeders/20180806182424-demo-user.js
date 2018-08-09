d4fbdf7cbfd5a3d6adfc500acba8986aa379d6c7
Fayoke Adeyina <fayokeadeyina@Fayokes-MacBook-Pro.local>
2018-08-09 14:14:43 +0100

    modify comment and reply models and migrateion

--- server/seeders/20180806182424-demo-user.js
+++ server/seeders/20180806182424-demo-user.js
@@ -6,13 +6,22 @@ module.exports = {
       lastName: 'Doe',
       username: 'unique',
       email: 'testseeder@test.com',
-      bio: `John Doe was born in 1977 when he arrived in Los Angeles. 
-        His previous life in Tennessee, 
-        Wisconsin & Baltimore was a great & fertile time but 
+      bio: `John Doe was born in 1977 when he arrived in Los Angeles.
+        His previous life in Tennessee,
+        Wisconsin & Baltimore was a great & fertile time but
         new music and social changes led him to events that created a life in art.`,
       image: 'https://www.image.com/example/image/john',
       createdAt: '2018-08-08 18:31:22.324',
-      updatedAt: '2018-08-08 18:31:22.324'
+      updatedAt: '2018-08-08 18:31:22.324',
+    },
+    {
+      firstName: 'Yomi',
+      lastName: 'Doe',
+      username: 'oyomi',
+      email: 'seayomi@gmail.com',
+      verified: true,
+      createdAt: '2018-08-01 21:54:49',
+      updatedAt: '2018-08-04 21:54:49',
     }
   ]),
   down: () => {
