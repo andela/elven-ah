a3e19b5cab64b1475ffd960f266a7508bdfd812f
Oyomi <seayomi@gmail.com>
2018-08-10 15:26:03 +0100

    feat(tagArticle): implement changes to tag model - modify tag model to accomodate articles id - modify the ArticleController.js file

--- server/models/tag.js
+++ server/models/tag.js
@@ -1,5 +1,9 @@
 export default (sequelize, DataTypes) => {
   const Tag = sequelize.define('Tag', {
+    articleId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
     title: {
       type: DataTypes.STRING,
       allowNull: false
