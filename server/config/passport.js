eec3d1ea1a90e7ee90eb2247695fd82d54dd72d6
Fayoke Adeyina <fayokeadeyina@Fayokes-MacBook-Pro.local>
2018-08-13 19:44:32 +0100

    add callback url to process.env

--- server/config/passport.js
+++ server/config/passport.js
@@ -6,12 +6,12 @@ const strategies = {
   google: {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://authors-haven-staging.herokuapp.com/api/auth/google/callback',
+    callbackURL: process.env.GOOGLE_CLIENT_CALLBACK_URL || '/api/auth/google/callback',
   },
   facebook: {
     clientID: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    callbackURL: 'https://authors-haven-staging.herokuapp.com/api/auth/facebook/callback',
+    callbackURL: process.env.FACEBOOK_CLIENT_CALLBACK_URL || '/api/auth/facebook/callback',
   },
   twitter: {
     consumerKey: 'get_your_own',
