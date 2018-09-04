daff47d8108c094c768010f04e57ac28d2d58216
CJ Odina <josephodina@gmail.com>
2018-09-04 13:14:20 +0100

    chore(versioning): version the API
    - modify all current routes to start with /api/v1/
    
    [Delivers #160127137]

---
+++ server/v1/controllers/VerifyController.js
@@ -0,0 +1,127 @@
+import models from '../../models';
+import JwtHelper from '../helpers/JwtHelper';
+import Mailer from '../helpers/Mailer';
+import emails from '../helpers/emailMessages';
+import AuthController from './AuthController';
+
+const { User } = models;
+
+/**
+ * This class handles all authentication operations
+ * such as signup, login and OAuth.
+ */
+class VerifyController {
+  /**
+   * @description Checks which property of the user exists already
+   * @param {Object} user The user object to be compared
+   * @param {String} email The email to be compared
+   * @param {String} username The username to be compared
+   * @returns Returns a message object specifying which property of
+   * the user is present in the user object
+   */
+
+  /**
+   * @description authenticate user with Google passport-Strategy
+   * @param {Object} user The user object to be compared
+   * @param {String} email The email to be compared
+   * @param {String} username The username to be compared
+   * @returns Returns a message object specifying which property of
+   * the user is present in the user object
+   */
+  static verifyEmail(req, res) {
+    const { email } = req.user || req.body;
+    const payload = { email };
+    const token = JwtHelper.createToken(payload, '24h');
+    const url = `${req.protocol}://${req.get('host')}/api/auth/verify?evc=${token}`;
+    const msg = emails.emailVerification(email, url);
+    Mailer.sendMail(msg)
+      .then((response) => {
+        if (response[0].statusCode === 202) {
+          res.status(201).json({
+            status: 'success',
+            message: req.emailVerificationMessage,
+          });
+        }
+        res.status(400).send('Unable to send email');
+      });
+  }
+
+  /**
+   * Verifies a user account
+   * @param {object} req the request object
+   * @param {object} res the response object
+   */
+  static activateUser(req, res, next) {
+    const token = req.query.evc;
+    if (token) {
+      const decoded = JwtHelper.verifyToken(token);
+      if (decoded) {
+        VerifyController.updateVerifiedStatus(decoded, req, res, next);
+      } else {
+        res.status(401).send({
+          status: 'fail',
+          message: 'This verification link is invalid or expired. Please try again'
+        });
+      }
+    } else {
+      res.status(401).send({
+        status: 'fail',
+        message: 'Please click the link sent to your email to verify your account.'
+      });
+    }
+  }
+
+  /**
+   * @description authenticate user with Facebook passport-Strategy
+   * @param {Object} user The user object to be compared
+   * @param {String} email The email to be compared
+   * @param {String} username The username to be compared
+   * @returns Returns a message object specifying which property of
+   * the user is present in the user object
+   */
+  static updateVerifiedStatus(decoded, req, res, next) {
+    User.update({ verified: true }, {
+      returning: true,
+      where: { email: decoded.email, verified: false }
+    })
+      .then(([, [user]]) => {
+        if (!user) {
+          return res.status(400).json({
+            status: 'error',
+            message: 'The account has already been verified.',
+          });
+        }
+        const status = { code: 200, message: 'Account successfully verified and logged in' };
+        AuthController.successResponse(status, user, res);
+      })
+      .catch(err => next(err));
+  }
+
+  /**
+   * Re-sends a verification email to the user
+   * @param {object} req the request object
+   * @param {object} res the response object
+   * @param {object} next the next middleware function
+   */
+  static resendVerificationEmail(req, res, next) {
+    const { email } = req.body;
+    User.find({ where: { email, verified: false } })
+      .then((user) => {
+        if (user) {
+          req.emailVerificationMessage = 'Email verification link re-sent successfully';
+          return VerifyController.verifyEmail(req, res, next);
+        }
+        return res.status(400).json({
+          status: 'error',
+          errors: {
+            user: [
+              'Invalid email or the account has already been verified.'
+            ],
+          },
+        });
+      })
+      .catch(err => next(err));
+  }
+}
+
+export default VerifyController;
---
+++ server/v1/tests/userSignup.test.js
@@ -0,0 +1,287 @@
+import { } from 'dotenv/config';
+import { describe, it } from 'mocha';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../..';
+import JwtHelper from '../helpers/JwtHelper';
+
+chai.should();
+chai.use(chaiHttp);
+
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  username: 'johnny',
+  email: 'testuser@test.com',
+  password: 'Qwertyui0p',
+  confirmPassword: 'Qwertyui0p',
+};
+const userWithAlreadyUsedEmail = {
+  firstName: 'John',
+  lastName: 'Doe',
+  username: 'johnnyllllee',
+  email: 'testuser@test.com',
+  password: 'Qwertyui0p',
+  confirmPassword: 'Qwertyui0p',
+};
+
+describe('User signup', () => {
+  const token = JwtHelper.createToken({ email: user.email }, '24h');
+  it('should create the user and send verification email', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send(user).end((err, res) => {
+      res.status.should.eql(201);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('success');
+      res.body.should.have.property('message').eql('User signup successful and verification email sent.');
+      done();
+    });
+  });
+
+  it('should re-send the verification email when requested by the user', (done) => {
+    chai.request(app).post('/api/v1/auth/verify').send({ email: user.email }).end((err, res) => {
+      res.status.should.eql(201);
+      res.body.should.be.an('object').with.property('message').include('Email verification link re-sent successfully');
+      done();
+    });
+  });
+
+  it('should not re-send the verification email when requested by the user  if no email is sent', (done) => {
+    chai.request(app).post('/api/v1/auth/verify').send({ username: user.username }).end((err, res) => {
+      res.status.should.eql(400);
+      res.body.should.be.an('object').with.property('errors');
+      done();
+    });
+  });
+
+  it('should activate the user if inactivated, and valid token supplied', (done) => {
+    chai.request(app).get(`/api/v1/auth/verify?evc=${token}`).end((err, res) => {
+      res.status.should.eql(200);
+      res.body.should.be.an('object').with.property('message').include('Account successfully verified and logged in');
+      res.body.should.have.nested.property('user.token');
+      done();
+    });
+  });
+
+  it('should return error if the user has already been verified', (done) => {
+    chai.request(app).get(`/api/v1/auth/verify?evc=${token}`).end((err, res) => {
+      res.status.should.eql(400);
+      res.body.should.be.an('object').with.property('message').include('The account has already been verified.');
+      done();
+    });
+  });
+
+  it('should return error if the user has already been verified', (done) => {
+    chai.request(app).get('/api/v1/auth/verify?evc=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNlYXlvbWlAZ21haWwuY29tIiw').end((err, res) => {
+      res.status.should.eql(401);
+      res.body.should.be.an('object').with.property('message').include('This verification link is invalid or expired. Please try again');
+      done();
+    });
+  });
+
+  it('should fail if a user tries to verify without a token', (done) => {
+    chai.request(app).get('/api/v1/auth/verify').end((err, res) => {
+      res.status.should.eql(401);
+      res.body.should.be.an('object').with.property('message').include('Please click the link sent to your email to verify your account.');
+      done();
+    });
+  });
+
+  it('should return 400 when a user does not provide a firstName', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send({
+      lastName: 'Yomi',
+      username: 'oyomi',
+      email: 'johndoe@gmail.com',
+      password: '8pcutTway',
+      confirmPassword: '8pcutTway',
+    }).end((req, res) => {
+      res.status.should.eql(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('errors');
+      res.body.errors.should.be.a('object');
+      res.body.errors.should.have.property('firstName').include('The firstName field is required.');
+      done();
+    });
+  });
+
+  it('should return 400 when a user does not provide a lastName', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send({
+      firstName: 'Doe',
+      username: 'Johnny',
+      email: 'johndoe@gmail.com',
+      password: 'Opcut2way',
+      confirmPassword: 'Opcut2way',
+    }).end((req, res) => {
+      res.status.should.eql(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('errors');
+      res.body.errors.should.be.a('object');
+      res.body.errors.should.have.property('lastName').include('The lastName field is required.');
+      done();
+    });
+  });
+
+  it('should return 400 when a user does not provide an email', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send({
+      firstName: 'John',
+      lastName: 'Doe',
+      username: 'Johnny',
+      password: 'Opcut2way',
+      confirmPassword: 'Opcut2way',
+    }).end((req, res) => {
+      res.status.should.eql(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('errors');
+      res.body.errors.should.be.a('object');
+      res.body.errors.should.have.property('email').include('The email field is required.');
+      done();
+    });
+  });
+
+  it('should return 400 when a user does not provide a username', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@gmail.com',
+      password: 'Opcut2way',
+      confirmPassword: 'Opcut2way',
+    }).end((req, res) => {
+      res.status.should.eql(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('errors');
+      res.body.errors.should.be.a('object');
+      res.body.errors.should.have.property('username').include('The username field is required.');
+      done();
+    });
+  });
+
+  it('should return 400 when a user does not provide a password', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@gmail.com',
+      username: 'Johnny',
+      confirmPassword: 'Opcut2way',
+    }).end((req, res) => {
+      res.status.should.eql(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('errors');
+      res.body.errors.should.be.a('object');
+      res.body.errors.should.have.property('password').include('The password field is required.');
+      done();
+    });
+  });
+
+  it('should return 400 when a user provides a password that is not up to 8 characters', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@gmail.com',
+      username: 'Johnny',
+      password: 'Opcut',
+    }).end((req, res) => {
+      res.status.should.eql(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('errors');
+      res.body.errors.should.be.a('object');
+      res.body.errors.should.have.property('password').include('The password must be at least 8 characters.');
+      done();
+    });
+  });
+
+  it('should return 400 when a user provides a password that does not contain an upper/lower case letter or number', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@gmail.com',
+      username: 'Johnny',
+      password: 'Opcutuyh',
+      confirmPassword: 'Opcutuyh',
+    }).end((req, res) => {
+      res.status.should.eql(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('errors');
+      res.body.errors.should.be.a('object');
+      res.body.errors.should.have.property('password').include('Password must contain an Upper case letter, a lower case letter and a number.');
+      done();
+    });
+  });
+
+  it('should return 400 when a user does not provide a confirmPassword', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@gmail.com',
+      username: 'Johhny',
+      password: 'Opcut2way',
+    }).end((req, res) => {
+      res.status.should.eql(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('errors');
+      res.body.errors.should.be.a('object');
+      res.body.errors.should.have.property('confirmPassword').include('The confirmPassword field is required.');
+      done();
+    });
+  });
+
+  it('should return 400 when a user provides a confirmPassword that is not equl to the password', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@gmail.com',
+      username: 'Johhny',
+      password: 'Opcut2way',
+      confirmPassword: 'Opcut2wau',
+    }).end((req, res) => {
+      res.status.should.eql(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('errors');
+      res.body.errors.should.be.a('object');
+      res.body.errors.should.have.property('confirmPassword').include('The confirmPassword and password fields must match.');
+      done();
+    });
+  });
+
+  it('should return 409 when a user with the email already exists', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send(userWithAlreadyUsedEmail).end((req, res) => {
+      res.status.should.eql(409);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('fail');
+      res.body.should.have.property('errors');
+      res.body.errors.should.be.a('object');
+      res.body.errors.should.have.property('email').include('User with email: testuser@test.com already exists.');
+      done();
+    });
+  });
+
+  it('should return 409 when a user with the username already exists', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send(user).end((req, res) => {
+      res.status.should.eql(409);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('fail');
+      res.body.should.have.property('errors');
+      res.body.errors.should.be.a('object');
+      res.body.errors.should.have.property('username').include('User with username: johnny already exists.');
+      done();
+    });
+  });
+
+  it('should return 409 when a user with the username and email already exists', (done) => {
+    chai.request(app).post('/api/v1/auth/signup').send(user).end((req, res) => {
+      res.status.should.eql(409);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('fail');
+      res.body.errors.should.have.property('username').include('User with username: johnny already exists.');
+      res.body.errors.should.have.property('email').include('User with email: testuser@test.com already exists.');
+      done();
+    });
+  });
+});
