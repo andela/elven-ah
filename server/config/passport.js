5fb9269804ac2138541b985444ba928c143ce77a
Chima Chukwuemeka <chukwuemekachm@gmail.com>
2018-09-16 14:41:10 +0100

    fix(exceptions): Fixes exceptions in create article
    - Modify the sequence of ArticleController line 74 through line 76
    - Add a return on new line 76
    - Modify redirectUrls in passport.js
    
    [Fixes #160538021]

--- server/config/passport.js
+++ server/config/passport.js
@@ -3,12 +3,12 @@ const strategies = {
   google: {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || '/api/auth/google/callback',
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://elven-ah-client.herokuapp.com/oauth2/google',
   },
   facebook: {
     clientID: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    callbackURL: process.env.FACEBOOK_CALLBACK_URL || '/api/auth/facebook/callback',
+    callbackURL: process.env.FACEBOOK_CALLBACK_URL || 'http://elven-ah-client.herokuapp.com/oauth2/facebook',
   },
   twitter: {
     consumerKey: 'get_your_own',
